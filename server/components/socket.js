55666ffec787fc87e1cab4e95bcedf4b02b58f3e
Emil Larsson <el223nc@student.lnu.se>
2018-06-12 07:56:17 +0200

    Epic fail again

--- server/components/socket.js
+++ server/components/socket.js
@@ -3,6 +3,10 @@ const socket = io => {
   io.on('connection', socket => {
     console.log('Connection')
   })
+
+  socket.on('msg', data => {
+    socket.emit('msg', data)
+  })
 }
 
 module.exports = socket
