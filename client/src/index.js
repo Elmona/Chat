5fa8aaad8b06d91d84f1bc5c9d90b81d70a15fcd
Emil Larsson <el223nc@student.lnu.se>
2018-06-12 09:30:11 +0200

    Cleaned some stuff

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import './App.css'
 import Chat from './Chat'
-import registerServiceWorker from './registerServiceWorker'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 ReactDOM.render(
   <MuiThemeProvider>
     <Chat />
   </MuiThemeProvider>
-  , document.getElementById('root'));
-
-registerServiceWorker();
+  , document.getElementById('root'))
