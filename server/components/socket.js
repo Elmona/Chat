0a92143eb3fd5c38cc79713768cf22341c47d87e
Emil Larsson <el223nc@student.lnu.se>
2018-06-14 11:50:57 +0200

    On connect send latest 50 messages to client

--- server/components/socket.js
+++ server/components/socket.js
@@ -2,10 +2,25 @@ const Message = require('../models/message')
 
 const socket = io => {
   io.on('connection', socket => {
-    console.log('Connection')
-
+    // On connect send the 50 latest messages to chat.
+    Message
+      .find({ channel: 'general' })
+      .sort({ 'date': 1 })
+      .limit(50)
+      .then(data => {
+        // Clean data don't send ip and user-agent to clients.
+        data = data.map(x => ({
+          id: x.id,
+          msg: x.msg,
+          date: x.date,
+          nick: x.nick,
+          channel: x.channel,
+          avatar: x.avatar,
+        }))
+        console.log('sending to client', data)
+        socket.emit('connection', { data: data})
+      })
     socket.on('msg', data => {
-      console.log(data)
       // Saving to database
       const message = new Message({
         date: data.date,
@@ -16,9 +31,8 @@ const socket = io => {
         ip: socket.handshake.headers['x-forwarded-for'],
         userAgent: socket.handshake.headers['user-agent']
       })
-      
-      message.save()
 
+      message.save()
       io.emit('msg', data)
     })
   })
